03bf529066704d6a976f51409ab883a341299b3c
owxnj1 <owenjohnson552@gmail.com>
2024-11-05 18:36:27 +0000

    Creating basic user registration and admin account

--- itapps/settings.py
+++ itapps/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'itreporting',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- itapps/urls.py
+++ itapps/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('itreporting.urls')),
+    path('register/', views.register, name='register'),
 ]
