ac1fbc5500d65d5ab557b300dbce10111bb166af
owxnj1 <owenjohnson552@gmail.com>
2024-11-06 14:57:11 +0000

    Profile page creation with login and logout functions

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -11,11 +11,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('itreporting:home')
+            messages.success(request, f'Your account has been created! Now you can login!')
+            return redirect('login')  
         else:
             messages.warning(request, 'Unable to create account.')
     else:
         form = UserRegisterForm()
     
     return render(request, 'users/register.html', {'form': form, 'title': 'Student Registration'})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html', {'title': 'Student Profile'})
